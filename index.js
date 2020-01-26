ab7613181ac8fbc57263b8bc9f97e2dc51b8dc7e
Anna Dybas <a.m.dybas@gmail.com>
2020-01-26 02:17:08 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) { "14:21"
+  let time = string.split(":")
+  let hour = parseInt(time[0])
+  if (hour < 12) {
+    return "Good Morning"
+  } else if (hour < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  let greetingNode = document.getElementById("greeting")
+  greetingNode.innerText = string
+}
